831bbdf9b4363a9bbe0c5c7893089ba14a2ee1bc
deepak2721 <deepak.baseapp@gmail.com>
2023-02-27 11:46:22 +0530

    angle to steps

---
+++ sdfdf.py
@@ -0,0 +1,7 @@
+input_number = 1
+input_min = 0
+input_max = 3
+output_min = 100
+output_max = 500
+result = (input_number-input_min)*(output_max-output_min)/(input_max-input_min)+output_min
+print(result)
